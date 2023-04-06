57a2396e70d7062058d9a21b6074101b7db7fd54
Ali Chaabane <milik9898>
2023-04-06 12:09:24 +0100

    init project

---
+++ src/main/java/com/tennis/academy/AcademyApplication.java
@@ -0,0 +1,51 @@
+package com.tennis.academy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.*;
+
+@SpringBootApplication
+public class AcademyApplication {
+
+	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(SpringBootApplication.class);
+		Connection connection = null;
+		Statement statement = null;
+		try {
+			logger.debug("Creating database if not exist...");
+			connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/", "postgres", "admin");
+			statement = connection.createStatement();
+			statement.executeQuery("SELECT count(*) FROM pg_database WHERE datname = 'tennisAcademy'");
+			ResultSet resultSet = statement.getResultSet();
+			resultSet.next();
+			int count = resultSet.getInt(1);
+
+			if (count <= 0) {
+				statement.executeUpdate("CREATE DATABASE tennisAcademy");
+				logger.debug("Database created.");
+			} else {
+				logger.debug("Database already exist.");
+			}
+		} catch (SQLException e) {
+			logger.error(e.toString());
+		} finally {
+			try {
+				if (statement != null) {
+					statement.close();
+					logger.debug("Closed Statement.");
+				}
+				if (connection != null) {
+					logger.debug("Closed Connection.");
+					connection.close();
+				}
+			} catch (SQLException e) {
+				logger.error(e.toString());
+			}
+		}
+		SpringApplication.run(AcademyApplication.class, args);
+	}
+
+}
